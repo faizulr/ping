410d3e5b1ee8d3450edec1d52062835baeb50528
faizulr <42220400+faizulr@users.noreply.github.com>
2025-08-22 16:26:37 +0300

    Create index.js

---
+++ main/index.js
@@ -0,0 +1,10 @@
+// Main entry point for the application
+console.log('Hello from index.js!');
+
+// Basic ping functionality
+function ping(host) {
+  console.log(`Pinging ${host}...`);
+  // Implementation would go here
+}
+
+module.exports = { ping };
